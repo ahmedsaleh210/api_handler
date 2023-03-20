41e2412dbce8f8092d42da501172a407ae471c7a
Your Name <ahmed.saleh212020@gmail.com>
2023-03-20 23:44:45 +0200

    initial commit

---
+++ lib/data/repositories/quote_repository.dart
@@ -0,0 +1,28 @@
+import 'package:api_handler/api_constants.dart';
+import 'package:api_handler/data/models/quote_model.dart';
+
+import '../data_resource/api_service.dart';
+
+class QuoteRepository
+{
+  final apiService = ApiService();
+
+  Future<QuoteModel> getRandomQuote() async
+  {
+    final response = await apiService.getData(ApiConstants.randomQuotePath);
+    return QuoteModel.fromJson(response.data);
+  }
+
+  Future<List<QuoteModel>> getTenRandomQuote() async
+  {
+    final response = await apiService.getData(ApiConstants.getTenRandomQuotePath);
+    return (response.data as List).map((e) => QuoteModel.fromJson(e)).toList();
+  }
+
+  Future<QuoteModel> getRandomQuoteByTitle({required String title}) async
+  {
+    final response = await apiService.getData(ApiConstants.randomQuoteByTitlePath,queryParams: {'title':title});
+    return QuoteModel.fromJson(response.data);
+  }
+
+}
---
+++ lib/presentation/api_screen.dart
@@ -0,0 +1,49 @@
+import 'package:api_handler/data/repositories/quote_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import '../data/data_resource/api_service.dart';
+import '../data/models/quote_model.dart';
+
+class ApiScreen extends StatefulWidget {
+  const ApiScreen({super.key});
+
+  @override
+  State<ApiScreen> createState() => _ApiScreenState();
+}
+
+class _ApiScreenState extends State<ApiScreen> {
+  final QuoteRepository quoteRepository = QuoteRepository();
+  List<QuoteModel> quotes = [];
+  bool isLoading = true;
+  @override
+  void initState() {
+    getRequest();
+    super.initState();
+  }
+
+  void getRequest() async {
+    quotes = await quoteRepository.getTenRandomQuote();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.separated(
+              itemBuilder: (context, index) => Card(
+                      child: ListTile(
+                    title: Text(quotes[index].quote),
+                  )),
+              separatorBuilder: (context, index) => const SizedBox(height: 10),
+              itemCount: quotes.length),
+    );
+  }
+}
